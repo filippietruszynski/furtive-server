92e06550da450a3cacc89cd93edc0b21dd658894
Filip Pietruszyński <filippietruszynski@pm.me>
2021-08-08 21:41:01 +0200

    Add initial config

---
+++ src/config/index.ts
@@ -0,0 +1,32 @@
+import {merge} from 'lodash'
+
+import devConfig from "./dev"
+import testConfig from "./test"
+
+const env = process.env.NODE_ENV || 'development';
+
+const baseConfig = {
+  env,
+  isDev: env === 'development',
+  isTest: env === 'testing',
+  secrets: {
+    jwtExp: '100d'
+  }
+}
+
+let envConfig;
+
+switch (env) {
+  case 'dev':
+  case 'development':
+    envConfig = devConfig
+    break
+  case 'test':
+  case 'testing':
+    envConfig = testConfig
+    break
+  default:
+    envConfig = devConfig
+}
+
+export default merge(baseConfig, envConfig)
---
+++ src/config/test.ts
@@ -0,0 +1,15 @@
+const testConfig = {
+  app: {
+    port: process.env.TEST_APP_PORT || 3000
+  },
+  secrets: {
+    jwt: process.env.TEST_JWT_SECRET || 'nulladiessinelinea'
+  },
+  db: {
+    host: process.env.TEST_DB_HOST || 'localhost',
+    port: process.env.TEST_DB_PORT || 27017,
+    name: process.env.TEST_DB_NAME || 'furtive-test'
+  }
+}
+
+export default testConfig
