efffb3def369bf0e62de80f78c4a76860cabea77
Filip Pietruszyński <filippietruszynski@protonmail.com>
2021-08-10 02:24:00 +0200

    Implement app.listen

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,23 @@
+import express from "express";
+import { json, urlencoded } from "body-parser";
+import morgan from "morgan";
+import cors from "cors";
+
 import config from "./config";
 
+export const app = express();
+
+app.disable("x-powered-by");
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
 export const start = () => {
-  console.log(config);
+  app.listen(config.app.port, () => {
+    console.log(
+      `Server listens on http://${config.app.host}:${config.app.port}/api`
+    );
+  });
 };
